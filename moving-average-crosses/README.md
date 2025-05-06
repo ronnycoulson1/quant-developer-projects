f05d827f8e79617aaa568c442c1d9d1dd9709eab
Ronny Coulson <ronnycoulson1@users.noreply.github.com>
2025-05-05 21:46:30 -0400

    main added

---
+++ moving-average-crosses/README.md
@@ -0,0 +1,3 @@
+# TODO: Describe strategy purpose
+# TODO: Include usage instructions
+# TODO: Add sample output (chart or stats)
---
+++ moving-average-crosses/data_loader.py
@@ -0,0 +1,3 @@
+#TODO: Write functions to download histolical stocks data
+#TODO: //example: use yfinace to fecth daily OHLC data 
+#TODO: Ensure functions retrun clean DataFrame for analysis 
